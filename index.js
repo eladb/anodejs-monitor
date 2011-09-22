e2f1d65a0c6eb942abf6993fc8bbf055d42877aa
eladb <elad.benisrael@gmail.com>
2011-09-22 12:03:47 +0300

    stopping ping due to shutdown of our mongodb endpoint

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var ping = require('./ping');
 var api = require('./api');
 
+
 var connection = 'mongo://157.55.174.56:10000/anodejs';
 
 // start the ping cycle.
-ping.start(connection);
+//the mongo connection is no longer valid so we are stopping ping
+//ping.start(connection);
 
 // start the dashboard web server
 api.listen(process.argv[3], connection);
